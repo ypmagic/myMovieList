28f09181b30173feb640ba2b2c3cfb302a86ffb5
Andrew Park <andrew@andrews-mbp-7.devices.brown.edu>
2019-05-04 11:42:09 -0400

    updated algorithms

--- src/main/java/recommend/Recommender.java
+++ src/main/java/recommend/Recommender.java
@@ -45,7 +45,7 @@ public final class Recommender {
     try {
       // list of data imdb ids
       List<String> data = new ArrayList<>();
-      ProcessBuilder pr = new ProcessBuilder("/anaconda3/bin/python", "src/"
+      ProcessBuilder pr = new ProcessBuilder("/Library/Frameworks/Python.framework/Versions/3.6/bin/python3", "src/"
           + "main/java/sorter/sorting.py", randomGenre);
       Process p = pr.start();
       p.waitFor();
